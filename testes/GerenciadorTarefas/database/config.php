acc23417d150b74162d42cee25b9480350ad22e7
KaiqueVfreitas <kaique.vieira2502eu@outlook.com>
2024-08-01 06:19:22 -0300

    Tentando adicionar um projeto feito em PHP de gerenciador de tarefas

--- testes/GerenciadorTarefas/database/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $servidor = "localhost";
-    $usuario = "root";
-    $senha = "!@#$";
-    $banco = "dbGerenciadorTarefasPHP";
-    $cmd = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
-?>
--- testes/GerenciadorTarefas/database/queryGerenciadorTarefas.sql
+++
@@ -1,9 +0,0 @@
-create database dbGerenciadorTarefasPHP; 
-use dbGerenciadorTarefasPHP;
-
-create table tbTarefas(
-	id int primary key auto_increment,
-	nomeTarefa varchar(255) not null, 
-	dataTarefa datetime null
-);
-
