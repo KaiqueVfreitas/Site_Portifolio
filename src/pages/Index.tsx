d4caf51dc4457ab551d661da5696631e19fcd064
KaiqueVfreitas <kaique.vieira2502eu@outlook.com>
2025-08-02 17:55:00 -0300

    merge: mesclando a branch InfoContatos com a main

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,15 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import HeroSection from '@/components/HeroSection';
+import { Contact } from "lucide-react";
+import ContactSection from "@/components/ContactSection";
 
 const Index = () => {
     return (
         <div>
             <Header />
             <HeroSection />
+            <ContactSection />
             <Footer />
         </div>
     )
