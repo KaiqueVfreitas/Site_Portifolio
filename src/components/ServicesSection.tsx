b9990151d7a29951d8ec6e03ef2b2eb52f4a06c9
KaiqueVfreitas <kaique.vieira2502eu@outlook.com>
2025-08-02 18:16:50 -0300

    star: inicio do desenvolvimento do components/ServicesSection.tsx

---
+++ src/components/ServicesSection.tsx
@@ -0,0 +1,11 @@
+import { Code, Palette, Smartphone, Globe, Database, Zap } from "lucide-react";
+
+
+const ServicesSection = () => {
+   return(
+      <h1>Serviços</h1>
+   )
+}
+
+
+export default ServicesSection
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,12 +3,14 @@ import Footer from "@/components/Footer";
 import HeroSection from '@/components/HeroSection';
 import { Contact } from "lucide-react";
 import ContactSection from "@/components/ContactSection";
+import ServicesSection from "@/components/ServicesSection";
 
 const Index = () => {
     return (
         <div>
             <Header />
             <HeroSection />
+            <ServicesSection />
             <ContactSection />
             <Footer />
         </div>
